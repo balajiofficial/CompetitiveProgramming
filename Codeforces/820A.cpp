21f8ba6a2dd4ffbd95cde340af90a3369ad14016
Balaji K <balajikyadav.official@gmail.com>
2020-12-16 15:38:51 +0530

    Update 820A.cpp

--- Codeforces/820A.cpp
+++ Codeforces/820A.cpp
@@ -9,10 +9,10 @@ int main() {
 	cin.tie(0);
 	int c, v0, v1, a, l;
 	cin >> c >> v0 >> v1 >> a >> l;
-	int ans = 1, d = v0;
-	while(c > 0) {
-		d = min(v1, a * ans - l);
-		c -= d;
+	int ans = 1, d = v0, e = v0;
+	while(e < c) {
+		d = min(v1, d + a);
+		e += d - l;
 		++ans;
 	}
 	cout << ans << "\n";
