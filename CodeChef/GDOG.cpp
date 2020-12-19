d6a0edff118e80176dc0f5cfab45c24a69ef2bc5
Balaji K <balajikyadav.official@gmail.com>
2020-12-19 11:16:31 +0530

    Create GDOG.cpp

---
+++ CodeChef/GDOG.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int t;
+	cin >> t;
+	while(t--) {
+		int n, k;
+		cin >> n >> k;
+		int ans = 0;
+		for(int i = 1; i <= k; ++i) {
+			ans = max(ans, n % i);
+		}
+		cout << ans << "\n";
+	}
+}
