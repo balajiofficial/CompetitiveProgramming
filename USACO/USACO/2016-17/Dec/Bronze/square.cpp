a1ab2cb52d9e725f04c23e330517fd5388bac58b
Balaji K <balajikyadav.official@gmail.com>
2021-02-23 23:11:37 +0530

    Update square.cpp

--- USACO/USACO/2016-17/Dec/Bronze/square.cpp
+++ USACO/USACO/2016-17/Dec/Bronze/square.cpp
@@ -4,20 +4,16 @@ using namespace std;
 
 #define ll long long
 
-struct rect {
-    int x1, x2, y1, y2;
+struct Rect {
+	int x1, x2, y1, y2;
 };
 
+Rect a, b;
+
 int main() {
-    ofstream cout ("square.out");
-    ifstream cin ("square.in");
-    rect a, b;
-    cin >> a.x1 >> a.y1 >> a.x2 >> a.y2;
-    cin >> b.x1 >> b.y1 >> b.x2 >> b.y2;
-    int minx = min(a.x1, b.x1);
-    int miny = min(a.y1, b.y1);
-    int maxx = max(a.x2, b.x2);
-    int maxy = max(a.y2, b.y2);
-    int c = max(maxx - minx, maxy - miny);
-    cout << c * c << "\n";
+	ofstream cout ("square.out");
+	ifstream cin ("square.in");
+	cin >> a.x1 >> a.y1 >> a.x2 >> a.y2;
+	cin >> b.x1 >> b.y1 >> b.x2 >> b.y2;
+	cout << pow(max(max(a.x2, b.x2) - min(a.x1, b.x1), max(a.y2, b.y2) - min(a.y1, b.y1)), 2) << "\n";
 }
