b8c85f754c54cd8ed6b75239453083975557b705
Balaji K <balajikyadav.official@gmail.com>
2021-11-03 16:54:42 +0530

    Update measurement.cpp

--- USACO/USACO/2017-18/Dec/Bronze/measurement.cpp
+++ USACO/USACO/2017-18/Dec/Bronze/measurement.cpp
@@ -2,60 +2,57 @@
 
 using namespace std;
 
-#define ll long long
+#define ll long long 
 
-
-struct cow {
+struct Cow {
+	int day, change;
 	string name;
-	int amt;
 };
 
+bool sortCows(Cow a, Cow b) {
+	return a.day < b.day;
+}
+
 int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 	ofstream cout ("measurement.out");
 	ifstream cin ("measurement.in");
-	int n;
+	int n, ans = 0;
 	cin >> n;
-	cow a[3];
-	a[0] = {"Bessie", 7};
-	a[1] = {"Elsie", 7};
-	a[2] = {"Mildred", 7};
-	map<int, pair<string, int>> s;
+	Cow cows[100];	
+	set<char> dis = {'b', 'e', 'm'};
 	for(int i = 0; i < n; ++i) {
-		string b;
-		int c, d;
-		cin >> c >> b >> d;
-		s[c] = make_pair(b, d);
+		cin >> cows[i].day >> cows[i].name >> cows[i].change;
 	}
-	int fin = 0;
-	bool z[3];
-	memset(z, 0, sizeof(z));
-	for(auto e : s) {
-		string l = e.second.first;
-		int num = e.second.second;
-		int high = 0;
-		for(int i = 0; i < 3; ++i) {
-			if (l == a[i].name) {
-				a[i].amt += num;
-			}
-			high = max(high, a[i].amt);
+	sort(cows, cows+n, sortCows);
+	int a = 0, b = 0, c = 0;
+	for(int i = 0; i < n; ++i) {
+		set<char> temp = dis;
+		if (cows[i].name == "Bessie") {
+			a += cows[i].change;
+		} else if (cows[i].name == "Elsie") {
+			b += cows[i].change;
+		} else {
+			c += cows[i].change;
 		}
-		bool ok = 0;
-		bool res[3];
-		memset(res, 0, sizeof(res));
-		for(int i=0; i < 3; ++i) {
-			if (a[i].amt == high) {
-				res[i] = 1;
-			} else {
-				res[i] = 0;
-			}
+		int maxMilk = max({a, b, c});
+		if (a == maxMilk) {
+			dis.insert('b');
+		} else if (dis.find('b') != dis.end()) {
+			dis.erase('b');
 		}
-		for(int i =0; i < 3; ++i) {
-			if (z[i] != res[i]) {
-				ok = 1;
-				z[i] = res[i];
-			}
+		if (b == maxMilk) {
+			dis.insert('e');
+		} else if (dis.find('e') != dis.end()) {
+			dis.erase('e');
 		}
-		fin += ok;
+		if (c == maxMilk) {
+			dis.insert('m');
+		} else if (dis.find('m') != dis.end()) {
+			dis.erase('m');
+		}
+		ans += (dis != temp);
 	}
-	cout << fin << "\n";
+	cout << ans << "\n";
 }
